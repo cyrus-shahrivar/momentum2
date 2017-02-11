f3dc50fe84606d49a8af69da3bb1405254741cd3
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2017-02-11 12:44:16 -0500

    Update quotes.js

--- data/quotes.js
+++ data/quotes.js
@@ -10,5 +10,9 @@ window.quotes = [
     {
         "author": "Unknown",
         "quote": "There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors."
+    },
+    {
+        "author": "Lao Tzu",
+        "quote": "Watch your thoughts; they become words. Watch your words; they become actions. Watch your actions; they become habit. Watch your habits; they become character. Watch your character; it becomes your destiny."
     }
 ];
