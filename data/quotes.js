e3b66311fde44e488d235277d0bcefeabb4347e4
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2017-02-26 21:04:52 -0500

    Update quotes.js

--- data/quotes.js
+++ data/quotes.js
@@ -14,5 +14,9 @@ window.quotes = [
     {
         "author": "Lao Tzu",
         "quote": "Watch your thoughts; they become words. Watch your words; they become actions. Watch your actions; they become habit. Watch your habits; they become character. Watch your character; it becomes your destiny."
+    },
+    {
+        "author": "Bruce Lee",
+        "quote": "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times."
     }
 ];
